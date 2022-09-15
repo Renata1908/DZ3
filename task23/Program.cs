00f004cf7558f19780d982d1cb23547c9d15b19e
Renata19 <renatalsa@mail.ru>
2022-09-15 20:42:48 +0700

    23

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N(N > 0):");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
+else
+{
+    Console.WriteLine("N должно быть больше 0!");
+}
