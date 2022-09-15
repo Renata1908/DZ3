ae7b3e177a82161ef33a5ef6c8b4abf4c3bb96a2
Renata19 <renatalsa@mail.ru>
2022-09-15 20:33:33 +0700

    19

---
+++ task19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 9999 && n < 100000)
+{
+
+if ((n / 10000) == (n % 10) && (n / 1000 % 10) == ((n % 100) / 10))
+{
+Console.WriteLine("Число является палиндромом");
+}
+else
+{
+Console.WriteLine("Число не является палиндромом");
+}
+}
+else
+{
+Console.WriteLine("Введено некорректное число!");
+}
